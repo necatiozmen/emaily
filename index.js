1fdfda979fa6f7ebd63ae7fe5ea72b66d8e26c46
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2018-06-21 12:43:47 +0300

    feat:passport strategy & folder structure changed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+require('./services/passport');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there1' });
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server running on Port 5000'));
