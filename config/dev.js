254c6b0fd35a0e88607afe87b6c19518b8aeca27
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2018-06-22 22:04:04 +0300

    config

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '102784452226-4bebokj70cd6br3gjn7c0e4qfsjek1k7.apps.googleusercontent.com',
+  googleClientSecret: 'RQ78yxnF-TcWtmuU2J9WfVMR',
+  mongoURI: 'mongodb://necati:156987987a@ds163850.mlab.com:63850/emaily-dev',
+  cookieKey: 'mkdmlsavsladnvakjsnvjslalv',
+};
