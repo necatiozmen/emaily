755ddbe3a3ccc11a4b69c7396e679835124319bf
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2018-07-22 21:15:50 +0300

    feat:Stripe add to client and server

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,8 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+const store = createStore(reducers,  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+window.__REDUX_DEVTOOLS_EXTENSION__(),
 applyMiddleware(reduxThunk));
 
 ReactDOM.render(
