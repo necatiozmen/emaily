8ccdf10caffff7401bae36bf3800d0a0c1232ae1
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2018-07-22 21:50:07 +0300

    feat:Middleware add

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import Payments from './Payments'
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -13,6 +13,9 @@ class Header extends Component {
       default:
         return [
           <li key="1"><Payments /></li>,
+          <li key="3" style ={{ margin: '0 10px' }}>
+            Credits : {this.props.auth.credits}
+          </li>,
           <li key="2"><a href="/api/logout">Logout</a></li>,
         ];
     }
